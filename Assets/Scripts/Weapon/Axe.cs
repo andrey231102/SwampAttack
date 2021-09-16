7f2dc447e3669754c24116cd4dde38bf8c8bb72c
andrey231102 <73721039+andrey231102@users.noreply.github.com>
2021-09-16 15:30:01 +0300

    Update Axe.cs

--- Assets/Scripts/Weapon/Axe.cs
+++ Assets/Scripts/Weapon/Axe.cs
@@ -6,11 +6,11 @@ public class Axe : MeleeWeapon
 {
     [SerializeField] private int _damage;
     [SerializeField] private float _attackRange;
-    [SerializeField] private LayerMask enemyLayers;
+    [SerializeField] private LayerMask _enemyLayers;
 
     public override void Smash(Transform attackPoint)
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, _attackRange, enemyLayers);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, _attackRange, _enemyLayers);
 
         foreach (Collider2D enemy in hitEnemies)
         {
